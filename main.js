97bc2c5eacb5b4e7d3380c8d790c1c80217260b4
DhyanJoshi121 <106371282+DhyanJoshi121@users.noreply.github.com>
2023-06-15 19:10:42 +0530

    finished

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 "use strict";
 
-const rateButtons = document.querySelector(".ratings");
+const ratingsDiv = document.querySelector(".ratings");
+const ratingButtons = document.querySelectorAll(".rate");
+const mainCard = document.querySelector(".main-card");
+const thanksCard = document.querySelector(".thanks-card");
 
-rateButtons.addEventListener("click", (e) => {
+ratingsDiv.addEventListener("click", (e) => {
   e.preventDefault();
-  document
-    .querySelectorAll(".rate")
-    .forEach((ele) => ele.classList.remove("rateClick"));
+  ratingButtons.forEach((ele) => ele.classList.remove("rateClick"));
   if (e.target !== e.currentTarget) e.target.classList.add("rateClick");
-  console.log(e.target);
+});
+
+document.querySelector(".submit").addEventListener("click", (e) => {
+  e.preventDefault();
+  let msg;
+  ratingButtons.forEach((ele) => {
+    ele.classList.contains("rateClick")
+      ? (msg = `You selected ${ele.innerText} out of 5`)
+      : false;
+  });
+  if (!msg) {
+    alert("please select rating");
+    return;
+  }
+  document.querySelector(".ratingOutOf5").insertAdjacentText("beforeend", msg);
+  mainCard.style.cssText += "display:none";
+  thanksCard.style.cssText += "display:flex";
 });
