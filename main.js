e630f58509cb513d8bff764c64edd0db923499cd
DhyanJoshi121 <106371282+DhyanJoshi121@users.noreply.github.com>
2023-06-15 13:37:51 +0530

    rate buttons with js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 "use strict";
 
-// const rateButtons = document.querySelector(".ratings");
+const rateButtons = document.querySelector(".ratings");
 
-// rateButtons.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   document
-//     .querySelectorAll(".rate")
-//     .forEach((ele) => ele.classList.remove("rateClick"));
-//   if (e.target !== e.currentTarget) e.target.classList.add("rateClick");
-//   console.log(e.target);
-// });
+rateButtons.addEventListener("click", (e) => {
+  e.preventDefault();
+  document
+    .querySelectorAll(".rate")
+    .forEach((ele) => ele.classList.remove("rateClick"));
+  if (e.target !== e.currentTarget) e.target.classList.add("rateClick");
+  console.log(e.target);
+});
